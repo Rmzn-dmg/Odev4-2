e869c969a3caa0726540315dfd0ab0b06d6f2117
Razaman Tunay <rtunay0@gmail.com>
2021-05-05 18:53:41 +0300

    Odev2

---
+++ campDay4/src/User/UserManager.java
@@ -0,0 +1,22 @@
+package User;
+
+public class UserManager implements UserService{
+
+	public void add(User user) {
+		// TODO Auto-generated method stub
+		System.out.println("kullanýcý eklendi " + user.getName());		
+	}
+
+	@Override
+	public void delete(User user) {
+		// TODO Auto-generated method stub
+		System.out.println("kullanýcý silindi " + user.getName());
+	}
+
+	@Override
+	public void update(User user) {
+		// TODO Auto-generated method stub
+		System.out.println("kullanýcý güncellendi " + user.getName());
+	}
+	
+}
